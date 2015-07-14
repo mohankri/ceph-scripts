782ed90d8e92c6c9f5f2d2e611555ad4976c3015
Krishna Mohan <mohankri@gmail.com>
2015-07-14 12:22:27 -0700

    Fix troubleshooting notes

--- ceph-install.sh
+++ ceph-install.sh
@@ -29,10 +29,10 @@ remove_dependencies()
 build_ceph()
 {
 	cd $CEPH_VER
-#	./install-deps.sh
-#	./autogen.sh
-#	./configure
-#	sudo make 
+	./install-deps.sh
+	./autogen.sh
+	./configure
+	sudo make 
 	sudo make install
 	cd ../
 }
@@ -55,9 +55,9 @@ update_ceph_script()
 	sudo sed -i s/"ETCDIR=\/usr\/local\/etc\/ceph"/"ETCDIR=\/etc\/ceph"/ /etc/init.d/ceph
 }
 
-#download_ceph
-#remove_dependencies
-#download_dependencies
+download_ceph
+remove_dependencies
+download_dependencies
 
 build_ceph
 install_ceph_script
